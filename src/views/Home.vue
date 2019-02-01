9b89ee5c4d1bcf192ad72fc28c6d4b076d15fd97
Justin Boyson <unclejustin@gmail.com>
2019-01-31 22:35:53 -0600

    Use product list and remove hello world.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ProductList/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductList from '@/components/ProductList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    ProductList,
   },
 }
 </script>
