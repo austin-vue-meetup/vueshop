d26233f2d9ca69d0dc7c12b10473f24fe32e3497
Justin Boyson <unclejustin@gmail.com>
2019-02-02 18:48:58 -0600

    Move initial products into variable. Add persisted state for products. Add INIT_PRODUCTS mutation.

--- src/store.js
+++ src/store.js
@@ -1,32 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import uuid from 'uuid'
+import createPersistedState from 'vuex-persistedstate'
+
+import constants from '@/constants'
 
 Vue.use(Vuex)
 
+const initialProducts = [
+  {
+    id: uuid(),
+    title: 'Shoes',
+    description: 'Snazzy shoes that go on your snazzy feet!',
+    price: 99.99,
+  },
+  {
+    id: uuid(),
+    title: 'Gloves',
+    description: 'Fancy gloves to protect the money makers.',
+    price: 29.99,
+  },
+  {
+    id: uuid(),
+    title: 'Pants',
+    description: 'Fancy dancy pantses.',
+    price: 49.99,
+  },
+]
+
 export default new Vuex.Store({
   state: {
-    products: [
-      {
-        id: uuid(),
-        title: 'Shoes',
-        description: 'Snazzy shoes that go on your snazzy feet!',
-        price: 99.99,
-      },
-      {
-        id: uuid(),
-        title: 'Gloves',
-        description: 'Fancy gloves to protect the money makers.',
-        price: 29.99,
-      },
-      {
-        id: uuid(),
-        title: 'Pants',
-        description: 'Fancy dancy pantses.',
-        price: 49.99,
-      },
-    ],
+    products: [],
   },
-  mutations: {},
+  mutations: {
+    [constants.INIT_PRODUCTS] (state) {
+      state.products = initialProducts
+    }
+  },
   actions: {},
+  plugins: [
+    createPersistedState({
+      key: 'vueshop',
+      paths: ['products'],
+    }),
+  ],
 })
