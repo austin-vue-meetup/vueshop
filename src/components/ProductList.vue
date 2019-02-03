c2cedbd2141a4bd0b98a89f444e8df5c92afdfc8
Justin Boyson <unclejustin@gmail.com>
2019-02-02 19:03:20 -0600

    Remove init products. Remove title.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,4 @@
 <script>
-import constants from '@/constants'
-
 export default {
   name: 'ProductList',
   computed: {
@@ -8,30 +6,19 @@ export default {
       return this.$store.state.products
     },
   },
-  methods: {
-    initProducts() {
-      this.$store.commit(constants.INIT_PRODUCTS)
-    },
-  },
 }
 </script>
 
 <template>
-  <div>
-    <h1>Behold Our Mighty Products!</h1>
-
-    <a href="#" class="btn btn-sm btn-outline-danger" @click="initProducts">Initialize Products</a>
-
-    <div class="card-deck mt-5">
-      <div v-for="product in products" :key="product.id" class="card">
-        <div class="card-body">
-          <h5 class="card-title">{{ product.title }}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{{ product.price }}</h6>
-          <p class="card-text">{{ product.description }}</p>
-        </div>
-        <div class="card-footer">
-          <a href="#" class="btn btn-primary btn-sm">Buy me!</a>
-        </div>
+  <div class="card-deck mt-5">
+    <div v-for="product in products" :key="product.id" class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ product.title }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{ product.price }}</h6>
+        <p class="card-text">{{ product.description }}</p>
+      </div>
+      <div class="card-footer">
+        <a href="#" class="btn btn-primary btn-sm">Buy me!</a>
       </div>
     </div>
   </div>
