3ae5082d266a4fa72a005978bf0001b669c2a30b
Justin Boyson <unclejustin@gmail.com>
2019-02-09 21:27:12 -0600

    Layout tweaks.

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -16,15 +16,33 @@ export default {
 
 <template>
   <div class="card-deck mt-5">
-    <div v-for="product in products" :key="product.id" class="card">
+    <div v-for="product in products" :key="product.id" :class="['card', $style.cardWidth]">
       <div class="card-body">
         <h5 class="card-title">{{ product.title }}</h5>
         <h6 class="card-subtitle mb-2 text-muted">{{ product.price }}</h6>
         <p class="card-text">{{ product.description }}</p>
       </div>
-      <div class="card-footer">
-        <a href="#" class="btn btn-primary btn-sm">Buy me!</a>
+      <div :class="['card-footer bg-primary text-white text-center', $style.cardFooter]">
+        Buy me!
       </div>
     </div>
   </div>
 </template>
+
+<style lang="scss" module>
+@media (min-width: 768px) {
+  .cardWidth {
+    max-width: 33%;
+  }
+}
+
+@media (min-width: 1500px) {
+  .cardWidth {
+    max-width: 25%;
+  }
+}
+
+.cardFooter {
+  cursor: pointer;
+}
+</style>
