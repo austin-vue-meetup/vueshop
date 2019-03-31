bb8de79a66d896d2f173fed4c11e475e9575f91c
Justin Boyson <unclejustin@gmail.com>
2019-03-31 16:14:47 -0500

    Routes for product list and edit

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Admin from './views/Admin.vue'
+import ProductList from '@/components/ProductList'
+import ProductEdit from '@/components/ProductEdit'
 
 Vue.use(Router)
 
@@ -17,7 +19,20 @@ export default new Router({
     {
       path: '/admin',
       name: 'admin',
+      redirect: { name: 'adminProduct' },
       component: Admin,
+      children: [
+        {
+          path: '/admin/product',
+          name: 'adminProduct',
+          component: ProductList,
+        },
+        {
+          path: '/admin/product/:id',
+          name: 'adminProductEdit',
+          component: ProductEdit,
+        },
+      ],
     },
   ],
 })
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Admin</h1>
+    <RouterView></RouterView>
   </div>
 </template>
